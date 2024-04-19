699a23a82ef71f3f403206cff6e32c84549a87fa
Julien Heng <julienheng0@gmail.com>
2024-04-19 08:32:03 +0200

    closes #16; Setup 404 page component finich

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 function NotFoundPage() {
-    console.log('Pagee introuvable');
+    return (
+        <main id="page-not-found">
+            <div id="page-not-found__wrap">
+                <h2>404</h2>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <Link to={'/'}>Retourner sur la page d’accueil</Link>
+            </div>
+        </main>
+    )
 }
 
 export default NotFoundPage;
